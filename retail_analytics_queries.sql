eb9a95614052ce457d1248b2d1e94d1a81967b04
Shanmukh Umesh Tata <tataumesh27@gmail.com>
2025-06-23 12:20:58 +0530

    Update retail_analytics_queries.sql

--- retail_analytics_queries.sql
+++ retail_analytics_queries.sql
@@ -120,27 +120,147 @@ select * from sales where tran_date_updates is null;
 update sales 
 set transactiondate = tran_date_updates;
 
--- select * from  sales limit 2000;
+# to summarize the total sales and quantities sold per product by the company
+	
+select ProductID,sum(QuantityPurchased) as TotalUnitsSold,
+sum(QuantityPurchased * Price) as TotalSales
+from Sales_transaction
+group by ProductID
+order by TotalSales desc;
 
-alter table sales
-drop column tran_date_updates;
 
--- 22-02-2025
+#  count the number of transactions per customer to understand purchase frequency
 
-use coding_ninjas;
+select CustomerID,first_value(transactiondate) 
+        over(partition by customerid order by transactiondate desc
+        rows between unbounded preceding and unbounded following) 
+            as last_purchase_date,
+ datediff(current_date(),
+    first_value(transactiondate) 
+        over(partition by customerid order by transactiondate desc
+        rows between unbounded preceding and unbounded following)
+ ) as days_since_last_purchase 
+ from Sales_transaction
+ order by days_since_last_purchase desc;
 
-create table prac_1(
- id int PRIMARY KEY,
- student_name varchar(20) NOT NULL,
- dob date NOT NULL
- );
- 
- create table prac_2(
-	student_id int ,
-    father_name varchar(20),
-    mother_name varchar(20),
-    FOREIGN KEY (student_id) REFERENCES prac_1(id)
-    );
-    
-    describe prac_2;
- 
+
+#  evaluate the performance of the product categories based on the total sales
+
+select p.Category, sum(s.quantitypurchased) as TotalUnitsSold,
+sum(s.quantitypurchased * s.price) as TotalSales
+from product_inventory p  
+inner join sales_transaction s 
+on p.productid = s.productid
+group by p.category
+order by totalsales desc;
+
+# find the top 10 products with the highest total sales
+
+select ProductID,TotalRevenue from (
+    select ProductID,sum(QuantityPurchased * Price) as TotalRevenue,
+    row_number() over(order by sum(QuantityPurchased * Price) desc) as rank_revenue
+    from sales_transaction
+    group by ProductID
+) as t
+where rank_revenue < 11;
+
+# ten products with the least amount of units sold
+
+select ProductID,sum(quantitypurchased) as TotalUnitsSold
+from sales_transaction
+group by ProductID
+having TotalUnitsSold > 0
+order by TotalUnitsSold 
+limit 10;
+
+# the sales trend
+
+select TransactionDate_updated as DATETRANS,
+count(*) as Transaction_count,
+sum(QuantityPurchased) as TotalUnitsSold,
+sum(quantityPurchased * price) as TotalSales
+from sales_transaction
+group by DATETRANS
+order by DATETRANS desc;
+
+
+# month on month growth rate of sales of the company
+
+with monthly_sales as (
+    select month(transactiondate_updated) as month,
+        sum(quantitypurchased * price) as total_sales
+        from sales_transaction
+        group by month
+)
+
+select month,total_sales,
+    lag(total_sales,1) over(order by month) as previous_month_sales,
+((total_sales - lag(total_sales) over(order by month))/(lag(total_sales,1) over(order by month)))*100
+as mom_growth_percentage
+from monthly_sales
+order by month;
+
+
+#  total amount spent by each customer along with number of transactions
+
+select CustomerID,count(*) as NumberOfTransactions,
+sum(Quantitypurchased * price) as TotalSpent
+from sales_transaction
+group by CustomerID
+having (NumberOfTransactions > 10) and (TotalSpent > 1000) 
+order by TotalSpent desc;
+
+
+#  customers who are occasional customers or have low purchase frequency in the company
+
+select CustomerID,count(*) as NumberOfTransactions,
+sum(QuantityPurchased * price) as TotalSpent
+from sales_transaction
+group by CustomerID
+having NumberOfTransactions <= 2
+order by NumberOfTransactions asc,TotalSpent desc;
+
+
+# total number of purchases made by each customer against each productID
+
+select CustomerID,ProductID,count(*) as TimesPurchased
+from Sales_transaction
+group by CustomerID,ProductID
+having TimesPurchased >1
+order by TimesPurchased desc;
+
+
+#  loyalty of the customer
+# duration between the first and the last purchase of the customer
+
+select CustomerID,
+min(str_to_date(transactiondate,"%Y-%m-%d")) as FirstPurchase,
+max(str_to_date(transactiondate,"%Y-%m-%d")) as LastPurchase,
+datediff(max(str_to_date(transactiondate,"%Y-%m-%d")),
+    min(str_to_date(transactiondate,"%Y-%m-%d"))) 
+    as DaysBetweenPurchases
+from Sales_transaction
+group by CustomerID
+having DaysBetweenPurchases > 0
+order by DaysBetweenPurchases desc;
+
+
+#  segments customers
+# based on the total quantity of products they have purchased
+#  target a particular segment for marketing
+
+with customer_segement as(
+    select c.Customerid,(case
+        when sum(s.quantitypurchased) between 1 and 9 then "Low"
+        when sum(s.quantitypurchased) between 10 and 30 then "Med"
+        when sum(s.quantitypurchased)>30 then "High"
+        end) as CustomerSegment
+        from customer_profiles c 
+        join sales_transaction s 
+        on c.customerid = s.customerid
+        group by c.customerid
+)
+
+select CustomerSegment,COUNT(*) from customer_segement
+group by CustomerSegment;
+
